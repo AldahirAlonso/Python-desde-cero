6c84129c6aa7ed0509eb96fce8aa2d91147d39dc
Aldahir Alonso <aldahir0603@hotmail.com>
2025-06-05 10:17:36 -0600

    Operaciones Basicas

---
+++ Operaciones.py
@@ -0,0 +1,27 @@
+name1 = 'Car Mar'
+name2 = "Carl Marcela"
+name3 = '''Carli 
+Marcela'''
+caracter = "c"
+
+print(name1)
+print(type(name1))
+print(name2)
+print(type(name2))
+print(name3)
+print(type(name3))
+print(type(caracter))
+
+name = "Aldahir Aldahir"
+last_name = "Alonso Alonso"
+
+print(name[0])
+print(name[1])
+print(name[-2])
+print(name * 5)
+print(name + " " + last_name)
+print(len(name))
+print(len(last_name))
+print(name.lower())
+print(name.upper())
+print(name.strip())
